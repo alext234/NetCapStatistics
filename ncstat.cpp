756f2ce1b1d2bb6ce86589113ce1414e6d3b346a
alex <u@ubuntu-14.04vm>
2016-12-09 11:36:44 +0800

    use the forked cxxopts

--- ncstat.cpp
+++ ncstat.cpp
@@ -12,9 +12,10 @@ int main (int argc, char* argv[])
 
     cxxopts::Options options(argv[0], "Analyze and output summary of network captures");
     options.add_options()
-        ("h,help", "display this help")
-        ("i,input", "pcap file, text file storing list of pcap files, or network interface", cxxopts::value<std::string> (input))
-        ("m,map", "mapping file", cxxopts::value<std::string>(mapfile))  ;
+        ("h,help", "display this help");
+
+    options.add_option ("","i", "input", "pcap file, text file storing list of pcap files, or network interface", cxxopts::value<std::string> (input),"",  true);
+    options.add_option ("", "m", "map", "mapping file", cxxopts::value<std::string>(mapfile), "", true)  ;
     options.parse(argc, argv);
 
     if (options.count("help"))
