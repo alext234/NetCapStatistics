42e322502fedcccbea8fa4a5a729a67b6198cfb4
alex <u@ubuntu-14.04vm>
2016-12-20 11:53:43 +0800

    split into hostgroup.cpp

--- hostgroup.h
+++ hostgroup.h
@@ -6,27 +6,19 @@
 #include <memory>
 
 struct shared_ptr_host_ipv4_compare {
-    bool operator() (const std::shared_ptr<Hostipv4>& lhs, const std::shared_ptr<Hostipv4>& rhs) const{
-        return *lhs < *rhs;
-    }
+    bool operator() (const std::shared_ptr<Hostipv4>& lhs, const std::shared_ptr<Hostipv4>& rhs)const;
 };
 
 class HostGroup {
 public:
     HostGroup(std::string name): name(name) {}
-    void addHost (std::shared_ptr<Hostipv4> host) {
-       hostSet.insert(host); 
-    }
-    std::shared_ptr<Hostipv4> find (std::string hostname) {
-        auto tempHostIp = std::make_shared<Hostipv4> ("0.0.0.0");
-        tempHostIp->setHostname(hostname);
-        auto it = hostSet.find(tempHostIp);
-        if (it==hostSet.end()) return nullptr;
-        return *it;
-    }
+    virtual void addHost (std::shared_ptr<Hostipv4> host) ;
+
+    std::shared_ptr<Hostipv4> find (std::string hostname) ;
+
     size_t size() { return hostSet.size();}
     std::string getName () {return name;}
-    auto getHosts() {return hostSet;}
+    std::set<std::shared_ptr<Hostipv4>, shared_ptr_host_ipv4_compare>  getHosts();
 protected:
     std::string name;
     std::set<std::shared_ptr<Hostipv4>, shared_ptr_host_ipv4_compare> hostSet;
