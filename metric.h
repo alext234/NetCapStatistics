9ab67556d7242e0dd0e2d1ee42672310e3a2545a
alex <u@ubuntu-14.04vm>
2016-12-28 16:04:23 +0800

    fix compile error with gcc-4.9

--- metric.h
+++ metric.h
@@ -2,9 +2,11 @@
 #define __METRIC__
 
 #include <cstdint>
-struct Metric {
+struct Metric {   
     uint64_t  txBytes=0;
     uint64_t  rxBytes=0;
+    Metric(){}
+    Metric(uint64_t tx, uint64_t rx) : txBytes{tx}, rxBytes{rx} {}
     void add(const Metric& a) { txBytes +=a.txBytes; rxBytes+=a.rxBytes; }
 };
 
